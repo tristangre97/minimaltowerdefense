a4fd34a5859ce6e755d4c67d4a218e243019e5e4
tristangre97 <tristangre97@gmail.com>
2017-08-01 21:52:19 -0400

    Add files via upload

---
+++ game/js/tgengine.js
@@ -0,0 +1,8 @@
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
+}
+
+document.getElementById('download').addEventListener('click', function() {
+    downloadCanvas(this, 'tower_defense', 'MTDScreenshot.png');
+}, false);
